70d590d1c3b03b325f773dab2705f5ba1dd1bc64
Garri0106 <115456362+Garri0106@users.noreply.github.com>
2022-11-27 18:30:38 +0100

    Add files via upload

--- Ej_5.py
+++ Ej_5.py
@@ -1,8 +1,13 @@
-num1 = int(input("Write the first number (base number):"))
-num2 = int(input("Enter the second number (exponent number):"))
+num1 = int(input("Enter the first number (base number):"))
+num2 = int(input("Enter the second number (exponent number):") or 0)
 
-def powerIt(num1, num2=0):
-    return (num1**num2)
-
-print(powerIt(2))
+def powerIt(num1, num2): 
+    if num1<0 or num2<0:
+        return (-num1**(num2))
+    elif num1 == 0:
+        return 0
+    elif num2 == 0:
+        return  1
+    else:
+        return (num1**num2)
 print(f"The result of raising {num1} to {num2} is {powerIt(num1, num2)}")
